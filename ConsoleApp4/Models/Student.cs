7b542c62bb70baa17d2cbb35915563c022edf0b8
IlkinAkhmed <tu7mp4w99@code.edu.az>
2024-03-28 12:07:58 +0400

    Homework but make class

---
+++ ConsoleApp4/Models/Student.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4.Models
+{
+    internal class Student
+    {
+        public string Name;
+        public string Surname;
+
+        public Student(string name, string surname)
+        {
+            Name = name;
+            Surname = surname;
+        }
+    }
+
+    class Person : Student
+    {
+        public string Group;
+        public byte Point; 
+        public bool IsGraduated;
+
+        public Person(string name, string surname, string group, byte point) : base(name, surname)
+        {
+            Group = group;
+            Point = point;
+            IsGraduated = point >= 65;
+        }
+
+        public void GetInfo()
+        {
+            Console.WriteLine($"Name: {Name} \nSurname: {Surname} \nGroup: {Group} \nPoint: {Point} \nIs Graduated: {(IsGraduated ? "He" : "Yox")}\n ");
+
+        }
+
+        public void CheckGraduation()
+        {
+            Console.WriteLine($"Graduation Status: {(IsGraduated ? "Bu telebe mezun olub" : "Mezun olmayib")}");
+        }
+
+        public void GetDiplomDegree()
+        {
+            if (Point < 65)
+            {
+                Console.WriteLine("Diplom dusmur");
+            }
+            else if (Point >= 65 && Point < 80)
+            {
+                Console.WriteLine("Adi diploma dushub");
+            }
+            else if (Point >= 80 && Point < 90)
+            {
+                Console.WriteLine("Seref diplomuna dushub");
+            }
+            else
+            {
+                Console.WriteLine("Yuksek Seref diplomuna dushub");
+            }
+        }
+    }
+}
---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,40 @@
+﻿using ConsoleApp4.Models;
+
+namespace ConsoleApp4;
+
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Person studentOne = new Person("Kamil", "Gahramanov", "AB106", 100);
+        studentOne.GetInfo();
+        studentOne.CheckGraduation();
+        studentOne.GetDiplomDegree();
+
+        Console.WriteLine();
+
+        Person studentTwo = new Person("Anar", "Amanli", "AB106", 80);
+        studentTwo.GetInfo();
+        studentTwo.CheckGraduation();
+        studentTwo.GetDiplomDegree();
+
+        Person studentThree= new Person("Emil", "Aliyev", "AB106", 70);
+        studentThree.GetInfo();
+        studentThree.CheckGraduation();
+        studentThree.GetDiplomDegree();
+
+        Person studentFour = new Person("Qerib", "Garibzade", "AB106", 60);
+        studentFour.GetInfo();
+        studentFour.CheckGraduation();
+        studentFour.GetDiplomDegree();
+
+        Person studentFive = new Person("Sahin", "Shahinzade", "AB106", 50);
+        studentFive.GetInfo();
+        studentFive.CheckGraduation();
+        studentFive.GetDiplomDegree();
+
+
+
+    }
+}
